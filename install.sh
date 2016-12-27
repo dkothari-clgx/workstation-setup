0a55625fcf814d81db8f0cf4166be458b82f0b96
Darshan Kothari <dkothari@corelogic.com>
2016-12-27 15:59:15 -0800

    added foundations for workstation-setup

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+function install_homebrew() {
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+    #need to check nothing went wrong
+}
+
+function install_git() {
+    brew install git
+}
+
+function install_java() {
+    brew cask install java
+    brew install gradle
+    brew install maven
+    brew install node
+    npm install --global grunt-cli
+    npm install --global gulp-cli
+}
+
+function install_chrome() {
+    brew cask install google-chrome
+}
+
+function install_cloudfoundry_tools() {
+    brew tap cloudfoundry/tap
+    brew install cf-cli
+    gem install cf-uaac
+    #need to figure how to automate pcfdev download
+    echo "Please go to https://network.pivotal.io/ to download PCFDev"
+}
+
+function install_virtualization() {
+    brew cask install docker
+    brew cask install virtualbox
+    brew cask install vagrant
+    vagrant plugin install vagrant-vbguest
+}
+
+function install_utilities() {
+    brew cask install 1password
+    brew cask install flycut
+    brew cask install iterm2
+    brew cask install shiftit
+}
+
+function install_communication_tools() {
+    brew cask install screenhero
+    brew cask install slack
+}
+
+function cloud_management_tools() {
+    brew install ansible
+    vagrant plugin install vagrant-puppet-install
+}
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source install.sh
+
+install_homebrew
+install_git
+install_java
+install_chrome
+install_virtualization
+install_utilities
+install_communication_tools
+install_cloudfoundry_tools
